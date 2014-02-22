2f82ec5ca09e3f99403638f727648d52be37b88d
Stephen John Russell <stephenjohnrussell@me.com>
2014-02-22 09:17:09 +0000

    OpenCV Setup and ready to go

--- Assignment 2: Photo Tour/main.cpp
+++ Assignment 2: Photo Tour/main.cpp
@@ -7,10 +7,24 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 int main(int argc, const char * argv[])
 {
+    
+    int h = 10;
+    int l = 20;
+    int t = 40;
+    
+    // Window & trackbars
+    namedWindow("blended", CV_WINDOW_AUTOSIZE);
+    createTrackbar("Canny High", "blended", &h, 199);
+    createTrackbar("Canny Low", "blended", &l, 199);
+    createTrackbar("Detection Threshold", "blended", &t, 99);
 
+    waitKey();
     // insert code here...
     std::cout << "Photo allignment!!\n";
     return 0;
