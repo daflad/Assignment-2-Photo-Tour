e273cfcc0d2eef3de09640a71c260ae4e8cd211d
Stephen John Russell <stephenjohnrussell@me.com>
2014-02-22 08:54:27 +0000

    Initial Commit

---
+++ Assignment 2: Photo Tour/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment 2: Photo Tour
+//
+//  Created by Stephen John Russell on 22/02/2014.
+//  Copyright (c) 2014 Stephen John Russell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
